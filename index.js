3bfbac17c1c1b85b257e11f14f1a638ebc81f5df
Erfan <erfana11@yahoo.com>
2022-03-22 17:38:10 +0430

    Fix port env.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
-const PORT = process.env.NODE_ENV || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server is up on port ' + PORT);
 });
